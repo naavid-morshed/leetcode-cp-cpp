fdca1c186eeed84f7fc9d73dd8aefcbb7f1c8dba
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-01-08 11:56:19 +0600

    prepared repo for neetcode spicific leetcode problems

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 
 #include "problems/2914/Sol2914.h"
-#include "problems/9999/Sol9999.h"
 using namespace std;
 
 int main() {
-    cout << Sol2914::minChanges("10110011") << endl;
-    Sol9999::toString();)
+    // cout << Sol2914::minChanges("10110011") << endl;
 }
