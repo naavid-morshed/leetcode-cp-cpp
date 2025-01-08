fdd6c8b40f578076dfa7e3f3bd32f72c8a563420
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-01-08 15:16:59 +0600

    neetcode leetcode 26 complete

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+#include "neetcode leetcode/26/Sol26.h"
 #include "problems/2914/Sol2914.h"
 using namespace std;
+#include <vector>
 
 int main() {
-    // cout << Sol2914::minChanges("10110011") << endl;
+    vector a = {1, 1, 1, 2, 3, 3, 3, 4, 4, 5, 5};
+
+    cout << Sol26::removeDuplicates(a) << endl;
 }
--- neetcode leetcode/26/Sol26.h
+++ neetcode leetcode/26/Sol26.h
@@ -4,5 +4,32 @@
 
 #ifndef SOL26_H
 #define SOL26_H
+#include <vector>
+using namespace std;
+#include <bits/stdc++.h>
 
+class Sol26 {
+public:
+    static int removeDuplicates(vector<int> &nums) {
+        const vector<int> temp = nums;
+        nums.clear();
+
+        int k = 1;
+
+        nums.push_back(temp[0]);
+        for (int i = 1; i < temp.size(); i++) {
+            if (temp[i] != temp[i - 1]) {
+                nums.push_back(temp[i]);
+                k++;
+            }
+        }
+
+        for (const int i: nums) {
+            cout << i << " ";
+        }
+        cout << endl;
+
+        return k;
+    }
+};
 #endif //SOL26_H
