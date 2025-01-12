4b76fd08d294e49ce3257f52bc41657b1b80c5a8
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-01-12 12:26:27 +0600

    staged repo for designing dynamic array & restructured repo

--- neetcode/designing dynamic array/SolDynamicArray.h
+++ neetcode/designing dynamic array/SolDynamicArray.h
@@ -5,4 +5,30 @@
 #ifndef SOLDYNAMICARRAY_H
 #define SOLDYNAMICARRAY_H
 
+class SolDynamicArray {
+public:
+    SolDynamicArray(int capacity) {
+    }
+
+    int get(int i) {
+    }
+
+    void set(int i, int n) {
+    }
+
+    void pushback(int n) {
+    }
+
+    int popback() {
+    }
+
+    void resize() {
+    }
+
+    int getSize() {
+    }
+
+    int getCapacity() {
+    }
+};
 #endif //SOLDYNAMICARRAY_H
