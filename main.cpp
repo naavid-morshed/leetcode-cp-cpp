426bff75e0b6b10dac269c938b8ebaa0a979a245
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-01-13 12:03:53 +0600

    neetcode dynamic array complete

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "neetcode leetcode/27/Sol27.h"
 #include "neetcode leetcode/27/Sol27Alt.h"
 #include "leetcode/2914/Sol2914.h"
+#include "neetcode/designing dynamic array/DynamicArray.h"
 using namespace std;
 #include <vector>
 
@@ -13,6 +14,20 @@ int main() {
     // cout << Sol26::removeDuplicates(a) << endl;
 
     // 27
-    vector b = {0, 1, 2, 2, 3, 0, 4, 2};
-    cout << Sol27Alt::removeElement(b, 2) << endl;
+    // vector b = {0, 1, 2, 2, 3, 0, 4, 2};
+    // cout << Sol27Alt::removeElement(b, 2) << endl;
+
+    // Dynamic Array
+    DynamicArray d = DynamicArray(5);
+    cout << d.getCapacity() << endl;
+    cout << d.getSize() << endl;
+
+    for (int i = 0; i < 10; i++) {
+        d.set(i, i + 1);
+    }
+
+    d.printValues();
+    cout << d.popback() << endl;
+    d.printValues();
+    cout << d.getSize() << endl;
 }
--- neetcode/designing dynamic array/DynamicArray.h
+++ neetcode/designing dynamic array/DynamicArray.h
@@ -2,33 +2,85 @@
 // Created by naavidmorshed on 1/12/25.
 //
 
-#ifndef SOLDYNAMICARRAY_H
-#define SOLDYNAMICARRAY_H
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
 
 class DynamicArray {
+    int *numArr;
+    int numArrSize;
+
 public:
     DynamicArray(int capacity) {
+        numArr = new int[capacity];
+        numArrSize = capacity;
     }
 
     int get(int i) {
+        return numArr[i];
     }
 
     void set(int i, int n) {
+        if (numArr[getCapacity() - 1] != 0) {
+            resize();
+        }
+
+        numArr[i] = n;
     }
 
     void pushback(int n) {
+        if (numArr[getCapacity() - 1] != 0) {
+            resize();
+        }
+
+        for (int i = 0; i < getCapacity(); i++) {
+            if (numArr[i] == 0) {
+                numArr[i] = n;
+                break;
+            }
+        }
     }
 
     int popback() {
+        const int poppedVal = numArr[getSize() - 1];
+        numArr[getSize() - 1] = 0;
+
+        return poppedVal;
     }
 
     void resize() {
+        int* temp = new int[getCapacity() * 2];
+
+        for (int i = 0; i < getCapacity(); i++) {
+            temp[i] = numArr[i];
+        }
+
+        numArr = temp;
+        numArrSize *= 2;
     }
 
     int getSize() {
+        int size = 0;
+
+        for (int i = 0; i < getCapacity(); i++) {
+            if (numArr[i] == 0) {
+                break;
+            }
+
+            size++;
+        }
+
+        return size;
     }
 
     int getCapacity() {
+        return numArrSize;
+    }
+
+    void printValues() {
+        for (int i = 0; i < getCapacity(); i++) {
+            cout << numArr[i] << " ";
+        }
+        cout << endl;
     }
 };
-#endif //SOLDYNAMICARRAY_H
+#endif //DYNAMIC_ARRAY_H
