01428368face9f3a2c533620b2d552bdeabe361d
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-01-13 16:08:57 +0600

    neetcode leetcode 1929 alternate solution

--- neetcode leetcode/1929/Sol1929Alt.h
+++ neetcode leetcode/1929/Sol1929Alt.h
@@ -4,5 +4,21 @@
 
 #ifndef SOL1929ALT_H
 #define SOL1929ALT_H
+using namespace std;
+#include <vector>
 
+class Sol1929Alt {
+public:
+    vector<int> getConcatenation(const vector<int> &nums, const int concatXtimes) {
+        vector<int> ans;
+
+        for (int times = 1; times <= concatXtimes; times++) {
+            for (const auto &num: nums) {
+                ans.push_back(num);
+            }
+        }
+
+        return ans;
+    }
+};
 #endif //SOL1929ALT_H
---
+++ neetcode/Valid Parentheses/ValidParentheses.h
@@ -0,0 +1,15 @@
+//
+// Created by naavidmorshed on 1/13/25.
+//
+
+#ifndef VALIDPARENTHESES_H
+#define VALIDPARENTHESES_H
+using namespace std;
+
+class ValidParentheses {
+public:
+    bool isValid(string s) {
+        string
+    }
+};
+#endif //VALIDPARENTHESES_H
