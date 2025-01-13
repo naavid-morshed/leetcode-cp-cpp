b35c34cd71305cfaed6fb485053de89bd87eeb56
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-01-13 15:07:51 +0600

    neetcode leetcode 1929 solved

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include "neetcode leetcode/26/Sol26.h"
 #include "neetcode leetcode/27/Sol27.h"
-#include "neetcode leetcode/27/Sol27Alt.h"
+#include "neetcode leetcode/1929/Sol1929.h"
 #include "leetcode/2914/Sol2914.h"
 #include "neetcode/designing dynamic array/DynamicArray.h"
 using namespace std;
@@ -17,14 +17,21 @@ int main() {
     // vector b = {0, 1, 2, 2, 3, 0, 4, 2};
     // cout << Sol27Alt::removeElement(b, 2) << endl;
 
-    // Dynamic Array
-    DynamicArray d = DynamicArray(1);
-    d.pushback(5);
-    cout << d.getCapacity() << endl;
-    d.printValues();
-    d.pushback(9);
-    cout << d.getCapacity() << endl;
-    d.printValues();
+    // // Dynamic Array
+    // DynamicArray d = DynamicArray(1);
+    // d.pushback(5);
+    // cout << d.getCapacity() << endl;
+    // d.printValues();
+    // d.pushback(9);
+    // cout << d.getCapacity() << endl;
+    // d.printValues();
 
+    vector v = {1,2,1};
+    auto s = Sol1929();
+    v = s.getConcatenation(v);
 
+    for (auto i : v) {
+        cout << i;
+    }
+    cout << endl;
 }
---
+++ neetcode leetcode/1929/Sol1929.h
@@ -0,0 +1,20 @@
+//
+// Created by naavidmorshed on 1/13/25.
+//
+
+#ifndef SOL1929_H
+#define SOL1929_H
+using namespace std;
+#include <vector>
+
+class Sol1929 {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+      vector<int> ans = nums;
+      for(const auto& num : nums) {
+        ans.push_back(num);
+        }
+        return ans;
+    }
+};
+#endif //SOL1929_H
