8e48dd6b6e8cb6e943f3f1b054193adcc6518718
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-01-21 10:51:50 +0600

    staged project for neetcode leetcode 155

---
+++ neetcode leetcode/155/Sol155.h
@@ -0,0 +1,38 @@
+//
+// Created by naavidmorshed on 1/21/25.
+//
+
+#ifndef SOL155_H
+#define SOL155_H
+class Sol155 {
+public:
+    Sol155() {
+
+    }
+
+    void push(int val) {
+
+    }
+
+    void pop() {
+
+    }
+
+    int top() {
+
+    }
+
+    int getMin() {
+
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+#endif //SOL155_H
