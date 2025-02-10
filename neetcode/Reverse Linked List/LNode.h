726da493105899c477f7f56568d403ea673ad94c
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-02-10 12:32:03 +0600

    staged repo for reversing linked list

---
+++ neetcode/Reverse Linked List/LNode.h
@@ -0,0 +1,8 @@
+//
+// Created by naavidmorshed on 2/10/25.
+//
+
+#ifndef LNODE_H
+#define LNODE_H
+
+#endif //LNODE_H
---
+++ neetcode/Reverse Linked List/solution.cpp
@@ -0,0 +1,19 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {
+    }
+
+    explicit ListNode(const int x) : val(x), next(nullptr) {
+    }
+
+    ListNode(const int x, ListNode *next) : val(x), next(next) {
+    }
+};
+
+class Solution {
+public:
+    ListNode *reverseList(ListNode *head) {
+    }
+};
