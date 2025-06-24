40640d8546d1a42e7ca5fd86e42d1a8e1fa9beb4
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-06-24 16:05:21 +0600

    solved 1472 and another alternate version

--- neetcode leetcode/1472/BrowserHistory.h
+++ neetcode leetcode/1472/BrowserHistory.h
@@ -35,13 +35,12 @@ public:
     }
 
     string forward(int steps) {
-        while (steps > 0 && !history.empty()) {
-            future.push(history.top());
-            history.pop();
+        while(steps > 0 && !future.empty()) {
+            history.push(future.top());
+            future.pop();
 
             steps--;
         }
-
         return history.top();
     }
 };
@@ -53,5 +52,4 @@ public:
  * string param_2 = obj->back(steps);
  * string param_3 = obj->forward(steps);
  */
-
 #endif //BROWSERHISTORY_H
