eaac78f03a878062c8f816ce3f07265841cb225a
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-07-10 18:19:16 +0600

    Staged merge sort, code on sunday july 13

--- neetcode/Merge Sort/MergeSort.h
+++ neetcode/Merge Sort/MergeSort.h
@@ -5,4 +5,34 @@
 #ifndef MERGESORT_H
 #define MERGESORT_H
 
+#include <utility>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Pair {
+public:
+    int key;
+    string value;
+
+    Pair(int key, string value) : key(key), value(std::move(value)) {
+    }
+};
+
+class Solution {
+public:
+    void divide() {
+
+    }
+
+    vector<Pair> mergeSort(vector<Pair> &pairs) {
+        if (pairs.size() == 1) {
+
+        } else {
+            divide()
+        }
+    }
+};
+
 #endif //MERGESORT_H
